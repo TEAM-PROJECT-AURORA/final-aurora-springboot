65dbe43b7587ba10bb26f69c2657664504b94524
osj <os133517@gmail.com>
2023-03-07 13:52:35 +0900

    # start

---
+++ src/main/java/com/root34/aurora/FinalAuroraSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.root34.aurora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalAuroraSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalAuroraSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/root34/aurora/FinalAuroraSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root34.aurora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalAuroraSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
