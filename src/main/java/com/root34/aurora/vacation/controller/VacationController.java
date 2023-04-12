cca71f1c4673a64d20382f76303b2374a2f86bc0
Hjaehong <zero5140@naver.com>
2023-04-12 23:24:09 +0900

    휴가 완료

--- src/main/java/com/root34/aurora/vacation/controller/VacationController.java
+++ src/main/java/com/root34/aurora/vacation/controller/VacationController.java
@@ -1,12 +1,16 @@
 package com.root34.aurora.vacation.controller;
 
+import com.root34.aurora.approval.dto.ApprovalDTO;
 import com.root34.aurora.common.ResponseDTO;
+import com.root34.aurora.vacation.dto.VacationDTO;
 import com.root34.aurora.vacation.service.VacationService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
 	@ClassName : VacationController
@@ -45,10 +49,26 @@ public class VacationController {
 	}
 
 	@PutMapping("/vacation/remain/{memberCode}")
-	public ResponseEntity<ResponseDTO> updateRemainVacation(@PathVariable int memberCode , @RequestParam( name= "vacationNo") int vacationNo ) {
+	public ResponseEntity<ResponseDTO> updateRemainVacation(@PathVariable int memberCode , @RequestBody VacationDTO vacationDTO) {
 
-		vacationService.updateRemainVacation(memberCode , vacationNo);
+		vacationService.calculateRemainVacation(vacationDTO);
 
-		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "수정", vacationService.selectVacation(memberCode)));
+		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "수정 완료", vacationService.selectVacation(memberCode)));
+	}
+
+	@PostMapping("/vacation/remain/{memberCode}")
+	public ResponseEntity<ResponseDTO> PostRemainVacation(@PathVariable int memberCode , @RequestBody @Valid VacationDTO vacationDTO) {
+		vacationDTO.setMemberCode(memberCode);
+		vacationService.PostRemainVacation(vacationDTO);
+
+		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "등록 성공", vacationService.selectVacation(memberCode)));
+	}
+
+	@PostMapping("/vacation/{memberCode}")
+	public ResponseEntity<ResponseDTO> insertRemainVacation(@PathVariable int memberCode) {
+
+		vacationService.insertVacation(memberCode);
+
+		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "등록 완료" , vacationService.selectVacation(memberCode)));
 	}
 }
